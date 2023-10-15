6d6a37c8d7af3860b89a139a4c8212f4955d97bd
tawansongsang <tawansongsang.k@gmail.com>
2023-10-15 17:33:26 +0700

    adding sql server  container

--- scripts/qr_dev.sh
+++ scripts/qr_dev.sh
@@ -3,16 +3,28 @@
 if [[ $1 = "start" ]];
 then
     # docker compose -f ./docker-compose-qr.yml $1
-    docker compose -f ./docker-compose-qr.yml $1 qr_rust;
+    # docker compose -f ./docker-compose-qr.yml $1 qr_rust;
+    docker compose -f ./docker-compose-qr.yml $1 qr_rust qr_db;
     # docker compose -f ./docker-compose-qr.yml $1 vue3
 elif [[ $1 = "stop" ]];
+then
+    docker compose -f ./docker-compose-qr.yml $1;
+elif [[ $1 = "up" ]];
+then
+    # docker compose -f ./docker-compose-qr.yml $1 qr_rust;
+    docker compose -f ./docker-compose-qr.yml $1 -d qr_rust qr_db;
+elif [[ $1 = "down" ]];
 then
     docker compose -f ./docker-compose-qr.yml $1;
 else
     echo "qr_dev.sh [COMMAND]";
     echo "COMMAND";
+    echo "    up:";
+    echo "        Creatine qr docker dev server images then start the server.";
+    echo "    down:";
+    echo "        Stopping and remove qr docker dev server containers.";
     echo "    start:";
-    echo "        Starting qr docker dev server";
+    echo "        Starting qr docker dev server.";
     echo "    stop:";
-    echo "        Stopping rq docker dev server";
+    echo "        Stopping rq docker dev server.";
 fi
